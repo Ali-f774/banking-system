8d2aff3c7d6895caa92178cc2387d83fa30643ee
Ali-f774 <alifaraji774@gmail.com>
2025-07-20 20:36:06 +0330

    add throws SQLException to BankingOperations Interface

--- src/java/BankingOperations.java
+++ src/java/BankingOperations.java
@@ -1,7 +1,9 @@
+import java.sql.SQLException;
+
 public interface BankingOperations {
-    void addAccount(int customerId,long accountNumber);
-    void deposit(long accountNumber,double amount);
-    void withdraw(long accountNumber,double amount);
-    void transfer(long originAccountNumber,long destinationAccountNumber,double amount);
-    void deleteAccount(long accountNumber);
+    void addAccount(int customerId,long accountNumber) throws SQLException;
+    void deposit(long accountNumber,double amount) throws SQLException;
+    void withdraw(long accountNumber,double amount) throws SQLException;
+    void transfer(long originAccountNumber,long destinationAccountNumber,double amount) throws SQLException;
+    void deleteAccount(long accountNumber) throws SQLException;
 }
