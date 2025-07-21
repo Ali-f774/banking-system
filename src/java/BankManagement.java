b559e5e887d5e4db0f63a319aae723cf42e4802f
Ali-f774 <alifaraji774@gmail.com>
2025-07-21 18:13:53 +0330

    fix database bugs

--- src/java/BankManagement.java
+++ src/java/BankManagement.java
@@ -1,9 +1,15 @@
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 
 public class BankManagement implements BankingOperations{
-    private String sql;
+    private String sql = "INSERT INTO transaction (account_number,type,amount,date,details) VALUES (?,?,?,?,?)";
+    private final PreparedStatement preparedStatement = DBConnection.getConnection().prepareStatement(sql);
+
+    public BankManagement() throws SQLException {
+    }
 
     @Override
     public void addAccount(int customerId, long accountNumber) throws SQLException {
@@ -18,6 +24,7 @@ public class BankManagement implements BankingOperations{
         sql = "UPDATE account SET balance = balance + "+amount+" WHERE account_number = "+accountNumber;
         Statement statement = DBConnection.getConnection().createStatement();
         statement.executeUpdate(sql);
+        addTransaction(accountNumber,"Deposit",amount, Date.valueOf(LocalDate.now()),"deposit money");
         System.out.println("Successful Deposit");
 
     }
@@ -27,6 +34,7 @@ public class BankManagement implements BankingOperations{
         sql = "UPDATE account SET balance = balance - "+amount+" WHERE account_number = "+accountNumber;
         Statement statement = DBConnection.getConnection().createStatement();
         statement.executeUpdate(sql);
+        addTransaction(accountNumber,"Withdraw",amount, Date.valueOf(LocalDate.now()),"withdraw money");
         System.out.println("Successful Withdraw");
     }
 
@@ -35,8 +43,10 @@ public class BankManagement implements BankingOperations{
         sql = "UPDATE account SET balance = balance - "+amount+" WHERE account_number = "+originAccountNumber;
         Statement statement = DBConnection.getConnection().createStatement();
         statement.executeUpdate(sql);
+        addTransaction(originAccountNumber,"Transfer",-amount, Date.valueOf(LocalDate.now()),"Transfer money");
         sql = "UPDATE account SET balance = balance + "+amount+" WHERE account_number = "+destinationAccountNumber;
         statement.executeUpdate(sql);
+        addTransaction(destinationAccountNumber,"Receive",amount, Date.valueOf(LocalDate.now()),"Receive money");
         System.out.println("Successful Transfer");
     }
 
@@ -46,5 +56,16 @@ public class BankManagement implements BankingOperations{
         Statement statement = DBConnection.getConnection().createStatement();
         statement.executeUpdate(sql);
         System.out.println("delete account is successfully");
+    }
+    private void addTransaction(long accountNumber, String type, double amount, Date date, String details) throws SQLException{
+        preparedStatement.setLong(1,accountNumber);
+        preparedStatement.setString(2,type);
+        preparedStatement.setDouble(3,amount);
+        preparedStatement.setDate(4,date);
+        preparedStatement.setString(5,details);
+        preparedStatement.addBatch();
+    }
+    public void registerTransaction() throws SQLException {
+        preparedStatement.executeBatch();
     }
 }
